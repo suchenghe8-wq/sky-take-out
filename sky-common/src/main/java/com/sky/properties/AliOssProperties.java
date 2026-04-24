ca4d826c7a67d3b88f0cfc0db3d5af625da17efd
liuminke <3171762900@qq.com>
2026-04-24 17:37:02 +0800

    阿里云文件上传

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "sky.alioss")
+@ConfigurationProperties(prefix = "sky.alioss")//配置属性类 读取配置文件的属性 封装成类
 @Data
 public class AliOssProperties {
 
