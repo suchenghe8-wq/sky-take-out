fc407fc7d3aead315c6e5b0693f3e4cce77c06b4
liuminke <3171762900@qq.com>
2026-04-25 19:23:29 +0800

    删除套餐业务代码

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -1,19 +1,20 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
-    /**
+/**
      * 套餐管理
      */
     @RestController
@@ -36,4 +37,28 @@ import org.springframework.web.bind.annotation.RestController;
             setmealService.saveWithDish(setmealDTO);
             return Result.success();
         }
+
+        /**
+         * 分页查询
+         * @param setmealPageQueryDTO
+         * @return
+         */
+        @GetMapping("/page")
+        @ApiOperation("分页查询")
+        public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
+            PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
+            return Result.success(pageResult);
+        }
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("批量删除套餐")
+    public Result delete(@RequestParam List<Long> ids){
+        setmealService.deleteBatch(ids);
+        return Result.success();
+    }
     }
