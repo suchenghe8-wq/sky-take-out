dd56db7b0dfccdeeb7b7cff0766d0089bc1fb4fe
liuminke <3171762900@qq.com>
2026-05-09 23:49:06 +0800

    清空购物车业务代码开发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
@@ -23,4 +24,12 @@ public interface ShoppingCartMapper {
     @Insert("insert into shopping_cart (name, user_id, dish_id, setmeal_id, image, amount, number, create_time) " +
             "values (#{name}, #{userId}, #{dishId}, #{setmealId}, #{image}, #{amount}, #{number}, #{createTime})")
     void insert(ShoppingCart shoppingCart);
+
+    /**
+     * 根据userid删除购物车数据
+     *
+     * @param userId
+     */
+    @Delete("delete from shopping_cart where user_id = #{userId}")
+    void deleteByUserId(Long userId);
 }
--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -44,6 +44,16 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     }
 
     /**
+     * 清空购物车
+     */
+    @Override
+    public void cleanShoppingCart() {
+        //获取到当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.deleteByUserId(userId);
+    }
+
+    /**
      * 添加购物车
      *
      * @param shoppingCartDTO
