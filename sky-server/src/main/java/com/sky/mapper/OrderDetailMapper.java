d4372b8cca077698dbf30e524ef88952df767707
liuminke <3171762900@qq.com>
2026-05-11 12:18:40 +0800

    查询历史订单功能代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -11,4 +11,11 @@ public interface OrderDetailMapper {
      * 批量插入订单明细数据
      */
     void insertBatch(List<OrderDetail> orderDetailList);
+
+    /**
+     * 根据订单id查询订单明细
+     * @param orderId
+     * @return
+     */
+    List<OrderDetail> getByOrderId(Long orderId);
 }
