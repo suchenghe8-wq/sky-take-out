ea17dc0640d730c950ee385920eee9bf303111e2
liuminke <3171762900@qq.com>
2026-05-11 23:46:57 +0800

    客户催单业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -506,6 +506,31 @@ public class OrderServiceImpl implements OrderService {
     }
 
     /**
+     * 客户催单
+     *
+     * @param id
+     */
+    @Override
+    public void reminder(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_NOT_FOUND);
+        }
+
+        //通过websocket向客户端浏览器推送消息 type orderId content
+        Map map = new HashMap();
+        map.put("type", 2);//1表示来单提醒 2表示客户催单
+        map.put("orderId", ordersDB.getId());
+        map.put("content", "订单号:" + ordersDB.getNumber());
+
+        String json = JSON.toJSONString(map);
+        WebSocketServer.sendToAllClient(json);
+    }
+
+    /**
      * 完成订单
      *
      * @param id
